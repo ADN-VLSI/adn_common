efad032c3ef860acf6b3f144bff949cecccc0a18
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-07-23 16:08:17 +0600

    modified:   source/adn_common_cdc_fifo.sv

--- source/adn_common_cdc_fifo.sv
+++ source/adn_common_cdc_fifo.sv
@@ -4,8 +4,8 @@
 
 @foez---bhai, describe the usage of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
-| REVISION | DATE       | AUTHOR          | DESCRIPTION                                            |
-|----------|------------|-----------------|--------------------------------------------------------|
+| REVISION | DATE       | AUTHOR              | DESCRIPTION                                            |
+|----------|------------|---------------------|--------------------------------------------------------|
 | 0.1      | YYYY-MM-DD | Ahasan Ullah Khalid | Initial version                                        |
 | 1.0      | YYYY-MM-DD | Ahasan Ullah Khalid | Stable release                                         |
 
