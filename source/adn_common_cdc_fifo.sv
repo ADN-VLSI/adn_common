3d245d6d6663c6e7cd7228f581dc43e760201699
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-07-27 14:52:01 +0600

    modified:   source/adn_common_cdc_fifo.sv

--- source/adn_common_cdc_fifo.sv
+++ source/adn_common_cdc_fifo.sv
@@ -1,29 +1,51 @@
 /*
-
+ 
 @foez---bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
-
+ 
 @foez---bhai, describe the usage of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
-
+ 
 | REVISION | DATE       | AUTHOR              | DESCRIPTION                                            |
 |----------|------------|---------------------|--------------------------------------------------------|
-| 0.1      | YYYY-MM-DD | Ahasan Ullah Khalid | Initial version                                        |
+| 0.1      | 2026-07-27 | Ahasan Ullah Khalid | Initial version                                        |
 | 1.0      | YYYY-MM-DD | Ahasan Ullah Khalid | Stable release                                         |
-
+ 
 Author : Ahasan Ullah Khalid (aukhalid02@gmail.com)
 This file is part of ADN-VLSI/adn_common
 Copyright (c) 2026 ADN Semiconductors
 Licensed under the MIT License
 See LICENSE file in the project root for full license information
-
+ 
 */
 
 // @foez---bhai, add comments to the parameters, ports
 module adn_common_cdc_fifo #(
-    // PARAMETERS
-    // LOCALPARAMS
-) (
+    //PARAMETERS
+    parameter int DATA_WIDTH = 32,
+    parameter int ADDR_WIDTH = 8,
+    parameter int SYNC_STAGES = 2,
+    parameter int ALMOST_FULL_THRESH = (1 << ADDR_WIDTH) - 2,
+    parameter int ALMOST_EMPTY_THRESH = 2
+  ) (
     // PORTS
-);
+
+    //Write Clock Domain
+    input  logic                   wr_clk,
+    input  logic                   wr_rst_n,
+    input  logic                   wr_en,
+    input  logic [DATA_WIDTH-1:0]  wr_data,
+    output logic                   full,
+    output logic                   almost_full,
+    output logic [ADDR_WIDTH:0]    wr_count,
+
+    //Read Clock Domain
+    input  logic                   rd_clk,
+    input  logic                   rd_rst_n,
+    input  logic                   rd_en,
+    output logic [DATA_WIDTH-1:0]  rd_data,
+    output logic                   empty,
+    output logic                   almost_empty,
+    output logic [ADDR_WIDTH:0]    rd_count
+  );
 
   // @foez---bhai, add comments to the functional blocks, signals, and submodules
 
@@ -64,8 +86,11 @@ module adn_common_cdc_fifo #(
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
 `ifdef SIMULATION
-  initial begin
-    if (DATA_WIDTH > 2) begin
+
+  initial
+  begin
+    if (DATA_WIDTH > 2)
+    begin
       $display("\033[1;33m%m DATA_WIDTH\033[0m");
     end
   end
