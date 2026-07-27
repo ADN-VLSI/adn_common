9e9a73448d2cfaa04f1f0e1bc1ce68cd527a1cf0
Md. Sakib Hasan Shawon <mdsakibhasanshawon20@gmail.com>
2026-07-27 16:24:54 +0600

    new file:   document/source/adn_common_edge_detect_des.svg modified:   local.f modified:   reuse.f deleted:    source/adn_common_edge_detect.sv

--- local.f
+++ local.f
@@ -1,2 +1,3 @@
-${ADN_COMMON}/testbench/adn_common_pipeline_tb.sv
 ${ADN_COMMON}/testbench/hello.sv
+${ADN_COMMON}/testbench/gray_counter_tb.sv
+${ADN_COMMON}/testbench/adn_common_pipeline_tb.sv
--- reuse.f
+++ reuse.f
@@ -1,3 +1,6 @@
 -i ${ADN_COMMON}/include
 ${ADN_COMMON}/interface/dummy_interface.sv
+${ADN_COMMON}/source/adn_common_ring_counter.sv
+${ADN_COMMON}/source/adn_common_edge_detect.sv
+${ADN_COMMON}/source/gray_counter.sv
 ${ADN_COMMON}/source/adn_common_pipeline.sv
