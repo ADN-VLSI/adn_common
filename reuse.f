90053899e4d711fac084baab46eb0269039c7aef
Md. Sakib Hasan Shawon <mdsakibhasanshawon20@gmail.com>
2026-07-28 17:26:29 +0600

    Edge_detect_complete

--- reuse.f
+++ reuse.f
@@ -2,5 +2,7 @@
 ${ADN_COMMON}/interface/dummy_interface.sv
 ${ADN_COMMON}/source/adn_common_ring_counter.sv
 ${ADN_COMMON}/source/adn_common_edge_detect.sv
-${ADN_COMMON}/source/gray_counter.sv
+${ADN_COMMON}/source/adn_common_jk_ff.sv
+${ADN_COMMON}/source/pr.sv
+${ADN_COMMON}/source/hs_combiner.sv
 ${ADN_COMMON}/source/adn_common_pipeline.sv
