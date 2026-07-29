683fb4ea837b3731ba98341d84dc7239ccdd5d79
Foez Ahmed <foez.official@gmail.com>
2026-07-29 18:18:04 +0600

    Process foez-bhai tagged files

--- reuse.f
+++ reuse.f
@@ -3,9 +3,9 @@ ${ADN_COMMON}/source/adn_common_cdc_fifo.sv
 ${ADN_COMMON}/source/adn_common_dual_port_ram.sv
 ${ADN_COMMON}/source/adn_common_edge_detect.sv
 ${ADN_COMMON}/source/adn_common_fifo.sv
+${ADN_COMMON}/source/adn_common_hs_combiner.sv
 ${ADN_COMMON}/source/adn_common_jk_ff.sv
 ${ADN_COMMON}/source/adn_common_pipeline.sv
 ${ADN_COMMON}/source/adn_common_synchronizer.sv
-${ADN_COMMON}/source/hs_combiner.sv
 -i ${ADN_COMMON}/include
 -i ${ADN_ENDEC}/include
