9de218b9dd71258194cbcf7aafc084a1bfc103ce
Annim <jannatannim@gmail.com>
2026-07-30 14:35:12 +0600

    modified:   source/adn_common_hs_counter.sv
    	modified:   source/adn_common_hs_counter.sv

--- source/adn_common_hs_counter.sv
+++ source/adn_common_hs_counter.sv
@@ -1,15 +1,15 @@
 /*
  
-@foez---bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
  
-@foez---bhai, describe the usage of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, describe the usage of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
  
 | REVISION | DATE       | AUTHOR          | DESCRIPTION                                            |
 |----------|------------|-----------------|--------------------------------------------------------|
-| 0.1      | 2026-07-28 | Annim | Initial version                                        |
-| 1.0      | 2026-07-30 | Annim | Stable release                                         |
+| 0.1      | 2026-07-27 | Annim | Initial version                                        |
+| 1.0      | 2026-07-29 | Annim | Stable release                                         |
  
-Author : Annim (jannatannim@gmail.com)
+Author : Annim Jannat (jannatannim@gmail.com)
 This file is part of ADN-VLSI/adn_common
 Copyright (c) 2026 ADN Semiconductors
 Licensed under the MIT License
