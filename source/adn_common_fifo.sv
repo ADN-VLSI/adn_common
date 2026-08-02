7b8e3ea028cd8cdcab96f1a81b215c1d5f7f6491
Foez Ahmed <foez.official@gmail.com>
2026-08-02 15:50:00 +0600

    modified:   source/adn_common_fifo.sv

--- source/adn_common_fifo.sv
+++ source/adn_common_fifo.sv
@@ -1,8 +1,8 @@
 /*
 
-@foez---bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
-@foez---bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
 | REVISION | DATE       | AUTHOR          | DESCRIPTION                                            |
 |----------|------------|-----------------|--------------------------------------------------------|
@@ -17,7 +17,7 @@ See LICENSE file in the project root for full license information
 
 */
 
-// @foez---bhai, add comments to the parameters, ports
+// @foez-bhai, add comments to the parameters, ports
 module adn_common_fifo #(
     parameter int DATA_WIDTH = 8,
     parameter int FIFO_SIZE  = 2,
@@ -37,7 +37,7 @@ module adn_common_fifo #(
     output logic [(2**FIFO_SIZE):0] count_o
 );
 
-  // @foez---bhai, add comments to the functional blocks, signals, and submodules
+  // @foez-bhai, add comments to the functional blocks, signals, and submodules
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // LOCALPARAMS GENERATED
