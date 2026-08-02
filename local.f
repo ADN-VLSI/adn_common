ab4f9fcd2ced067a89e81709d8ca3e790c1c003c
Adnan Sami Anirban <adnananirban259@gmail.com>
2026-08-02 14:43:01 +0600

    Reuse F.P ARB 	deleted:    document/source/adn_common_priority_selector.md 	deleted:    document/source/adn_common_priority_selector_top.svg 	modified:   local.f 	modified:   source/adn_common_address_decoder.sv 	deleted:    source/adn_common_priority_selector.sv

--- local.f
+++ local.f
@@ -1,5 +1,4 @@
 -i ${ADN_COMMON}/testbench
 ${ADN_COMMON}/testbench/adn_common_edge_detect_tb.sv
 ${ADN_COMMON}/testbench/adn_common_pipeline_tb.sv
-${ADN_COMMON}/testbench/hello.sv
 ${ADN_COMMON}/testbench/adn_common_address_decoder_tb.sv
