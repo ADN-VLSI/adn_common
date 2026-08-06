b02b685a95bae3d8dbb67ca179b835044e733a9e
MD Sakhawat Hossain Sabbir <sabbirone939@gmail.com>
2026-08-06 13:07:29 +0600

    HS Counter TB Done

--- local.f
+++ local.f
@@ -1,7 +1,9 @@
 -i ${ADN_COMMON}/testbench
-${ADN_COMMON}/testbench/adn_common_address_decoder_tb.sv
 ${ADN_COMMON}/testbench/adn_common_edge_detect_tb.sv
-${ADN_COMMON}/testbench/adn_common_fixed_priority_arbiter_tb.sv
-${ADN_COMMON}/testbench/adn_common_jk_ff_tb.sv
-${ADN_COMMON}/testbench/adn_common_pipeline_tb.sv
 ${ADN_COMMON}/testbench/adn_common_round_robin_arbiter_tb.sv
+${ADN_COMMON}/testbench/adn_common_jk_ff_tb.sv
+${ADN_COMMON}/testbench/adn_common_hs_counter_tb.sv
+${ADN_COMMON}/testbench/adn_common_fixed_priority_arbiter_tb.sv
+${ADN_COMMON}/testbench/adn_common_hs_countertb.sv
+${ADN_COMMON}/testbench/adn_common_pipeline_tb.sv
+${ADN_COMMON}/testbench/adn_common_address_decoder_tb.sv
