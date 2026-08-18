d18ade968b466dce36094092009619667aa1ecba
Foez Ahmed <foez.official@gmail.com>
2026-08-19 00:22:29 +0600

    auto-update

--- reuse.f
+++ reuse.f
@@ -16,9 +16,9 @@ ${ADN_COMMON}/source/adn_common_hs_combiner.sv
 ${ADN_COMMON}/source/adn_common_hs_counter.sv
 ${ADN_COMMON}/source/adn_common_jk_ff.sv
 ${ADN_COMMON}/source/adn_common_parity_generator.sv
-${ADN_COMMON}/source/adn_common_pipeline.sv
 ${ADN_COMMON}/source/adn_common_pipeline_join.sv
 ${ADN_COMMON}/source/adn_common_pipeline_split.sv
+${ADN_COMMON}/source/adn_common_pipeline.sv
 ${ADN_COMMON}/source/adn_common_priority_encoder.sv
 ${ADN_COMMON}/source/adn_common_range_checker.sv
 ${ADN_COMMON}/source/adn_common_ring_counter.sv
