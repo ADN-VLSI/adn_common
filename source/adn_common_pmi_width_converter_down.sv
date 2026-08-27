ca36bb80085b5a12dd8a3a5066d05ac0cba94250
Shuparna Haque <sheikhshuparna3108@gmail.com>
2026-08-27 17:41:55 +0600

    DOC UPDATE 	modified:   source/adn_common_pmi_width_converter_down.sv

--- source/adn_common_pmi_width_converter_down.sv
+++ source/adn_common_pmi_width_converter_down.sv
@@ -1,8 +1,8 @@
 /*
 
-@foez---bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
-@foez---bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
 | REVISION | DATE       | AUTHOR          | DESCRIPTION                                            |
 |----------|------------|-----------------|--------------------------------------------------------|
@@ -17,7 +17,7 @@ See LICENSE file in the project root for full license information
 
 */
 
-// @foez---bhai, add comments to the parameters, ports
+// @foez-bhai, add comments to the parameters, ports
 module adn_common_pmi_width_converter_down #(
     // PARAMETERS
     parameter type s_req_t = logic,
@@ -35,7 +35,7 @@ module adn_common_pmi_width_converter_down #(
     output m_req_t m_pmi_req_o,
     input  m_rsp_t m_pmi_rsp_i
 );
-  // @foez---bhai, add comments to the functional blocks, signals, and submodules
+  // @foez-bhai, add comments to the functional blocks, signals, and submodules
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // LOCALPARAMS GENERATED
